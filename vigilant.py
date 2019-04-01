8b48c7fcf1ecdd63027b1463f95d0e1ec07b934f
Brian Tracy <brian.tracy33@gmail.com>
2019-03-31 23:44:37 -0400

    added graphical output

--- vigilant.py
+++ vigilant.py
@@ -12,11 +12,11 @@ import datetime
 import sys
 import json
 
-
 DATE_FORMAT = '%a %b %d %Y'     # 'Sun Mar 17 2019'
 LOG_FILE = '/Users/briantracy/Desktop/projects/vigilant/file.json'
 
 def add_checkpoint(data, interval):
+    print('{} {} minutes have passed'.format(datetime.datetime.now(), interval))
     ts = today_string()
     prev = data.get(ts, 0)
     data[ts] = prev + interval
@@ -39,6 +39,9 @@ def write_log(log, fname):
 def today_string():
     return datetime.datetime.now().strftime(DATE_FORMAT)
 
+def sort_keys(data):
+    return sorted(data.keys(), key=lambda s: datetime.datetime.strptime(s, DATE_FORMAT))
+
 def print_table(data):
     if not data:
         print('no data to display')
@@ -48,13 +51,15 @@ def print_table(data):
              '-'*(len('00h00m')+2) + '+'
     def table_time(mins):
         return '{:02d}h{:02d}m'.format(mins // 60, mins % 60)
-    def row(l, r):
-        print('| {} | {} |'.format(l, r))
+    def row(l, r, mins):
+        print('| {} | {} | {} '.format(l, r, '*' * (mins // 30)))
 
-    for key in sorted(data.keys(), key=lambda s: datetime.datetime.strptime(s, DATE_FORMAT)):
+    for key in sort_keys(data):
         print(horz)
-        row(key, table_time(data[key]))
+        row(key, table_time(data[key]), data[key])
     print(horz)
+
+
 
 def main(args):
     log = read_log(LOG_FILE)
